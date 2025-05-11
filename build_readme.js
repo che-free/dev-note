fe7e6b614137a0a3f8da891fe22202534dfbf2bf
cheyongjae <cyj@enjoydev.com>
2025-05-12 02:33:20 +0900

    하위 디렉토리 전체에 README.md 파일 생성하도록 수정

--- build_readme.js
+++ build_readme.js
@@ -1,160 +1,159 @@
 /**
- * 소스 디렉토리 하위 Markdown 파일 리스트를 이용해서 README.md 파일을 생성한다.
+ * 저장소 하위 Markdown 파일들을 이용해서 각 디렉토리에 README.md 파일을 만든다.
  *
  * ● 실행 방법
  * $ node build_readme.js
  */
 
-const BASE_URL = "https://github.com/che-free/dev-note/blob/main/";
-const BOM = '\ufeff';
-const DEFAULT_ORDER_NO = 9999;
-
 const fs = require('fs');
 const path = require('path');
 
-// 디렉토리, 파일 정렬 순서 설정
-const orders = {
-    //"Java": 100,
-    //"TIL": 200,
-    //"MySQL(MariaDB)": 30,
-    //"Javascript/CoinMarketCap API.md": DEFAULT_ORDER_NO - 10,
-};
+// 저장소 루트 경로
+const ROOT_PATH = __dirname;
+
+// GitHub URL
+const BASE_URL = "https://github.com/che-free/dev-note/blob/main";
+
+// UTF-8 BOM
+const BOM = '\ufeff';
+
+// Default 정렬값
+const DEFAULT_ORDER_NO = 5000;
+
+// 디렉토리, 파일 메타 정보
+const META_DATA = {
+    "/": { orderNo: null, title: "개발 메모" },
+    //"/MySQL(MariaDB)": { orderNo: null, title: "MYSQL" },
+}
+
 
 // 메인 프로그램 시작
 main();
+
 
 /**
  * 프로그램 메인
  */
 function main() {
-    const allFiles = getAllMarkdownFiles();
-    const dirs = [];
+    console.log("ROOT_PATH :: " + ROOT_PATH);
 
-    // 1레벨 디렉토리 리스트 생성
-    allFiles.forEach((file) => {
-        const depth = file.file.split("/").length;
-        const dirname = file.dirname;
+    // 디렉토리 리스트 조회
+    const allDirs = getAllMarkdownDirectories();
 
-        if (depth == 2) {
-            if (!dirs.includes(dirname)) {
-                dirs.push(dirname);
-            }
+    //console.log(JSON.stringify(allDirs, null, 4));
+
+    allDirs.forEach((dir1) => {
+        if (dir1.totalCount > 0) {
+            const dirs = [];
+
+            allDirs.forEach((dir2) => {
+                if (dir1.path == "/" || dir2.path == dir1.path || dir2.path.startsWith(dir1.path + "/")) {
+                    dirs.push(dir2);
+                }
+            });
+
+            // README.md 파일 생성
+            makeReadmeFile(dir1, JSON.parse(JSON.stringify(dirs)));
         }
     });
+}
 
-    // 파일 리스트 정렬
-    allFiles.sort(function (a, b) {
-        if (a.orderNo != b.orderNo) {
-            return a.orderNo > b.orderNo ? 1 : -1;
-        }
 
-        if (a.dirname != b.dirname) {
-            return a.dirname > b.dirname ? 1 : -1;
-        }
-
-        if (a.depth != b.depth) {
-            return a.depth > b.depth ? 1 : -1;
-        }
-
-        return a.file > b.file ? 1 : 0;
-    });
-
-//    allFiles.forEach((file) => {
-//        console.log("files :: ", file);
-//    });
-
-    // 디렉토리 리스트 정렬
-    dirs.sort(function (a, b) {
-        const order1 = orders[a] ? orders[a] : DEFAULT_ORDER_NO;
-        const order2 = orders[b] ? orders[b] : DEFAULT_ORDER_NO;
-
-        if (order1 != order2) {
-            return order1 > order2 ? 1 : -1;
-        }
-
-        return a > b ? 1 : 0;
-    });
-
-//    dirs.forEach((dir) => {
-//        console.log("dir :: ", dir);
-//    });
+/**
+ * README.md 파일을 만든다.
+ */
+function makeReadmeFile(readmeDir, dirs) {
+    //console.log("README :: " + readmeDir.path);
 
     let content = "";
 
-    content += makeHeader();
+    // TODO: 디렉토리 정렬
 
     dirs.forEach((dir) => {
-        const files = [];
+        let title = null;
+        let basePath = null;
 
-        allFiles.forEach((file) => {
-            if (file.file.startsWith(dir + "/")) {
-                files.push(file);
-            }
-        });
+        if (dir.depth == readmeDir.depth) {
+            title = getHeaderText(dir);
 
-        content += makeDirSection(dir, files);
+            content += `# ${title}` + "\r\n";
+        } else if (dir.depth == readmeDir.depth + 1 && dir.totalCount > 0) {
+            title = getHeaderText(dir);
+
+            content += "\r\n\r\n" + `## ${title}` + "\r\n";
+        }
+
+        if (dir.depth == readmeDir.depth) {
+            basePath = dir.path.split("/").slice(0, readmeDir.depth + 1).join("/");
+        } else {
+            basePath = dir.path.split("/").slice(0, readmeDir.depth + 2).join("/");
+        }
+
+        if (dir.files.length > 0) {
+            dir.files.forEach((file) => {
+                content += makeLinkItem(basePath, file);
+            });
+        }
     });
 
-    saveReadmeFile(content);
+    content += "\r\n";
+
+    saveReadmeFile(readmeDir.path, content);
 }
 
 
 /**
- * README.md 파일 헤더 영역
+ * 경로명으로 Readme 헤더 텍스트를 구한다.
  */
-function makeHeader() {
-    let content = "";
+function getHeaderText(dir) {
+    let meta = META_DATA[dir.path];
+    let title = dir.path.split("/").pop();
 
-    content += "# 개발 노트";
-    content += "\r\n" + "";
+    if (meta && meta.title) {
+        title = meta.title;
+    }
 
-    return content;
+    return title;
 }
 
 
 /**
- * README.md 디렉토리별 파일 리스트 영역
+ * Markdown 파일의 링크 텍스트를 만든다.
  */
-function makeDirSection(dir, files) {
-    let content = "";
+function makeLinkItem(basePath, file) {
+    let text = file.substr(basePath.length + 1);;
+    text = text.substr(0, text.length - 3);
 
-    console.log("** dir :: ", dir);
-    console.log("files :: ", files);
+    //let url = BASE_URL + encodeURIComponent(file.file);
+    let url = BASE_URL + encodeURI(file);
 
-    content += "\r\n" + `## ${dir}`;
+    // [MySQL_SQL_code_snippets](https://github.com/che-free/dev-note/blob/main/MySQL(MariaDB)/MySQL_SQL_code_snippets.md)
+    html = `- [${text}](${url})` + "\r\n";
 
-    files.forEach((file) => {
-        let text = file.file.substr(dir.length + 1);;
-        text = text.substr(0, text.length - 3);
-
-        //let url = BASE_URL + encodeURIComponent(file.file);
-        let url = BASE_URL + encodeURI(file.file);
-
-        // [MySQL_SQL_code_snippets.md](https://github.com/che-free/dev-note/blob/main/MySQL(MariaDB)/MySQL_SQL_code_snippets.md)
-        content += "\r\n" + `- [${text}](${url})`;
-    });
-
-    content += "\r\n" + "";
-    content += "\r\n" + "";
-
-
-    return content;
+    return html;
 }
 
 
 /**
- * 디렉토리 하위에서 Markdown 전체 파일 리스트를 리턴한다.
+ * 저장소 하위 전체 디렉토리 및 디렉토리별 Markdown 파일 리스트를 리턴한다.
  */
-function getAllMarkdownFiles() {
-    let ignoreFiles = [".git"];
+function getAllMarkdownDirectories() {
+    let ignoreFiles = [".git", "README.md"];
 
     const walkSync = (dir, files = []) => {
         const filenames = fs.readdirSync(dir);
+        const dirInfo = {
+            path: (dir == ROOT_PATH ? "/" : dir.substring(ROOT_PATH.length).replaceAll("\\", "/")),
+            depth: null,
+            totalCount: 0,
+            count: 0,
+            files: [],
+        }
 
         for (const filename of filenames) {
-            const file = path.join(dir, filename).replaceAll("\\", "/");
+            const file = path.join(dir, filename);
 
-            if (ignoreFiles.includes(file)) {
+            if (ignoreFiles.includes(filename)) {
                 continue;
             }
 
@@ -163,38 +162,58 @@ function getAllMarkdownFiles() {
             if (stats.isFile()) {
                 const dirname = path.dirname(file);
                 const extname = path.extname(file);
-                const orderNo = orders[file] ? orders[file] : DEFAULT_ORDER_NO;
+                const filePath = file.substring(ROOT_PATH.length).replaceAll("\\", "/");
+
 
                 if (extname != ".md") {
                     continue;
                 }
 
-                files.push({
-                    depth: file.split("/").length,
-                    dirname: dirname,
-                    filename: filename,
-                    file: file,
-                    orderNo: orderNo,
-                });
+                dirInfo.count++;
+                dirInfo.files.push(filePath);
             } else if (stats.isDirectory()) {
                 walkSync(file, files);
             }
         }
 
+        dirInfo.depth = (dirInfo.path == "/" ? 0 : dirInfo.path.split("/").length - 1);
+
+        files.push(dirInfo);
+
         return files;
     }
 
-    return walkSync(".\\");
+    // 재귀호출로 디렉토리 리스트 조회
+    dirs = walkSync(ROOT_PATH);
+
+    // 각 디렉토리의 하위 파일 개수 합계 계산
+    dirs.forEach((dir1) => {
+        dirs.forEach((dir2) => {
+            if (dir1.path == "/" || dir2.path == dir1.path || dir2.path.startsWith(dir1.path + "/")) {
+                dir1.totalCount += dir2.files.length;
+            }
+        });
+    });
+
+    // 디렉토리 리스트 정렬
+    dirs.sort(function (a, b) {
+        return a.path > b.path ? 1 : -1;
+    });
+
+    return dirs;
 }
 
 
 /**
  * README.md 파일에 저장한다.
  */
-function saveReadmeFile(content) {
+function saveReadmeFile(readmePath, content) {
     const fs = require('fs');
-    const fileName = "README.md";
+    const fileName = path.resolve(ROOT_PATH + readmePath + "/README.md");
+
+    console.log("README :: " + fileName);
 
     fs.writeFileSync(fileName, BOM + content);
 }
+
 
